f9b5636bbe5f7d07ca6351dc9cd1e265ddda887b
aisana0131 <aisana0131@yahoo.com>
2024-06-15 18:57:08 +0000

    thank you Simon

--- homework3/task1.sh
+++ homework3/task1.sh
@@ -1,60 +1,55 @@
 #!/bin/bash
 
 # Make sure the script is being executed with superuser privileges.
-
 if [[ "${PID}" -ne 0 ]]; then
 	echo "Please run as a root or with superuser privileges"
 	exit
 fi
 
-# If the user doesn’t supply at least one argument, then give them help.
 
-if [ $# -le 0 ]; then
-	echo "You have to provide at least one argument"
-	exit
-fi 
 
 # The first parameter is the user name.
-
 username="${1}"
 
-# The rest of the parameters are for the account comments.
 
+# The rest of the parameters are for the account comments.
 comment="${@}"
 
 # Generate a password.
+password="$(date +%s%N | sha256sum | head -c36)" &> /dev/null
 
-$ genpasswd
+# If the user doesn’t supply at least one argument, then give them help.
+while [ $# = 0 ]; do
+        read -p "Provide username: " username
+        read -p "Add a comment: " comment
+	break
+done
+
 
 # Create the user with the password.
-
-useradd -p $(openssl passwd -1 password) username
+useradd -m  ${username} -c "${comment}"
 
 # Check to see if the useradd command succeeded.
-
-if [ $? -eq 0 ]; then
-        echo Succeeded
-else
-        echo Failed
+if [[ $? -ne 0 ]]; then
+	echo "Could not add the user, sorry"
+	exit 1
 fi
 
 # Set the password.
 
-passwd -e $1
+echo ${password} | passwd --stdin ${username} &> /dev/null
 
 # Check to see if the passwd command succeeded.
-
 if [[ $? -ne 0 ]]; then
-        echo "Did not set a password"
+        echo "Could not set the password, sorry"
         exit 1
 fi
-
 # Force password change on first login.
-
 passwd -e ${username}  &> /dev/null
 
 # Display the username, password, and the host where the user was created.
-
+echo 
 echo "Username: ${username}"
 echo "Password: ${password}"
 echo "Host: ${HOSTNAME}"
+echo
